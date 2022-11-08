88c9b549aab00e280e506b181eb2a457cd4d84c8
emilybrehmer-rxa <emily.brehmer@rxa.io>
2022-11-08 09:28:23 -0500

    initial commit

---
+++ components/dataDecomp.js
@@ -0,0 +1,21 @@
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+today = yyyy+'-'+ mm + '-' + dd;
+// today = mm+'-'+dd+'-'+yyyy
+
+async function setupDate(){
+    var harvestQuarter = 1;
+    const setupQuery = 'Select harvestdate, harvestquarter from harvest where harvestdate = \'' + today + '\' Group BY harvestdate, harvestquarter';
+    
+    let promise = domo.post('/sql/v1/harvest', setupQuery, {contentType: 'text/plain'})
+            .then(function(harvest){
+
+                return harvest.rows[0][1];       
+                });
+        
+    harvestQuarter = await promise;
+    return harvestQuarter;
+}
---
+++ js/app.js
@@ -0,0 +1,76 @@
+var query;
+
+(async () => {
+  let quarter = await setupDate();
+  let select = ['goalhoursactual', 'qtrdayselapsed', 'harvesthoursbilled', 'goalhoursactuallegacy', 'qtrworkdays']
+  let where = [`harvestquarter= ${quarter}`]
+  query = `/data/v1/harvest?fields=${select.join()}
+                  &filter=${where.join()}
+  `;
+})();
+
+
+function calculate(keyPressed){
+  calculateHours(keyPressed);
+}
+
+function calculateHours(days){
+  var qtrdayselapsed =  [];
+  var goalhoursactual = [];
+  var harvesthoursbilled = [];
+  var goalhoursactuallegacy = [];
+  var qtrworkdays = [];
+  
+  domo.get(query)
+  .then(function(data){
+    
+    data.forEach(entry => {
+      qtrdayselapsed.push(entry.qtrdayselapsed);
+      goalhoursactual.push(entry.goalhoursactual);
+      harvesthoursbilled.push(entry.harvesthoursbilled);
+      goalhoursactuallegacy.push(entry.goalhoursactuallegacy);
+      qtrworkdays.push(entry.qtrworkdays);
+    })
+    
+    var maxQtrDaysElapsed = maxValue(qtrdayselapsed);
+    var sumHarvestHoursBilled = sumValue(harvesthoursbilled);
+    var maxQtrWorkDays = maxValue(qtrworkdays);
+    var sumGoalHoursActualLegacy = sumValue(goalhoursactuallegacy);
+
+    console.log("maxQtrWorkDays: ", maxQtrWorkDays);
+    console.log("maxQtrDaysElapsed: ", maxQtrDaysElapsed);
+    console.log("sumGoalHoursActualLegacy: ", sumGoalHoursActualLegacy);
+    console.log("sumHarvestHoursBilled: ", sumHarvestHoursBilled);
+
+    var adjDaysLeftInQtr = (maxQtrWorkDays - maxQtrDaysElapsed - days);
+    document.getElementById("adj-days-left").innerHTML = adjDaysLeftInQtr.toFixed(0);  
+    
+    var totalHoursToHitGoal = sumGoalHoursActualLegacy - sumHarvestHoursBilled
+    document.getElementById("total-hours-to-hit-goal").innerHTML = totalHoursToHitGoal.toFixed(0);
+
+    var avgHoursToHitGoal = (sumGoalHoursActualLegacy - sumHarvestHoursBilled) / (adjDaysLeftInQtr)
+    document.getElementById("new-avg-hours-needed").innerHTML = avgHoursToHitGoal.toFixed(1);
+  });
+}
+
+function maxValue(arr){
+  var max = 0;
+  arr.forEach(entry => {
+    if(entry >= max){
+      max=entry;
+    }
+  });
+
+  return max;
+}
+
+function sumValue(arr){
+  var sum = 0;
+  arr.forEach(entry => {
+    if(entry != null){
+      sum += entry;
+    }
+  });
+
+  return sum;
+}
